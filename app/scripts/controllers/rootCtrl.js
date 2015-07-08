a4ab3ed65b0cb3ca358e33547df2a2f339e674f6
farzanaalam <farzana@live.com>
2015-07-08 16:51:31 -0400

    Adding Date using databinding

--- app/scripts/controllers/rootCtrl.js
+++ app/scripts/controllers/rootCtrl.js
@@ -11,7 +11,10 @@ angular.module('hiqApp')
   .controller('rootCtrl', function ($scope, getUserService) {
     getUserService.getUser().$promise.then(function(response) {
       $scope.userName = response.userName;
-    });
+    })
+
+    $scope.getDate = new Date();
+    $scope.date= $scope.getDate.toDateString();
 
     console.log('Root state');
   });
